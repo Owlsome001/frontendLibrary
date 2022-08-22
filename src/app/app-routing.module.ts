995df516e48d21ca37b1c697a51d7ba387f87f55
Owlsome001 <103942607+Owlsome001@users.noreply.github.com>
2022-08-22 11:47:28 +0200

    First Upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AttendanceComponent } from './layout/content/attendance/attendance.component';
-import { BookComponent } from './layout/content/book/book.component';
-import { ListComponent } from './layout/content/list/list.component';
-import { LayoutComponent } from './layout/layout.component';
 
-const routes: Routes = [
-  {
-    path: '',
-    component: LayoutComponent,
-  },
-  {
-    path: 'details/:isbn',
-    component: BookComponent,
-  },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
