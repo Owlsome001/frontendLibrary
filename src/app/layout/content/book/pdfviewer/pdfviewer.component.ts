d36cb4d83ce45177aa5388bc54c80fee9b87f8f1
Owlsome001 <103942607+Owlsome001@users.noreply.github.com>
2022-09-03 17:21:47 +0200

    Update pdfviewer.component.ts

--- src/app/layout/content/book/pdfviewer/pdfviewer.component.ts
+++ src/app/layout/content/book/pdfviewer/pdfviewer.component.ts
@@ -1,18 +1,23 @@
 import { BookInfo } from 'src/app/models/BookInfo';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Pipe, PipeTransform} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BooksService } from 'src/app/services/http.service';
+import { DomSanitizer } from "@angular/platform-browser";
+import {  } from '@angular/core';
 
 @Component({
   selector: 'app-pdfviewer',
   templateUrl: './pdfviewer.component.html',
   styleUrls: ['./pdfviewer.component.css']
 })
-export class PdfviewerComponent implements OnInit {
+export class PdfviewerComponent implements OnInit,PipeTransform  {
   book!:BookInfo
   pdfFilePath!: string;
 
   constructor(private activatedRouter:ActivatedRoute, private http: BooksService) { }
+  transform(value: any, ...args: any[]) {
+    throw new Error('Method not implemented.');
+  }
   ngOnInit() {
     this.getpdf()
   }
